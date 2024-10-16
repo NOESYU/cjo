01fb53b5aeb67abd7fc80315f7a15d20773796c0
Jeon. <86655940+NOESYU@users.noreply.github.com>
2024-10-16 15:54:47 +0900

    헤더 반응형, 메인슬라이드 레이아웃

--- js/script.js
+++ js/script.js
@@ -11,3 +11,25 @@ $(".menu>li").mouseover(function(){
 $(".menu>li").mouseout(function(){
   $(this).children(".sub_bg").stop().slideUp(300);
 })
+
+
+$(".m_nav").hide();
+$(".m_btn").click(function(){
+  $(".m_nav").fadeIn(300);
+  $(".m_menu_box").addClass("active");
+})
+$(".m_close, .m_nav_bg").click(function(){
+  $(".m_nav").fadeOut(300);
+  $(".m_menu_box").removeClass("active");
+  $(".m_sub_bg").hide();
+})
+
+/* 브랜드 클릭시 m_sub_bg 보이기 */
+/* this 로 접근해서 브랜드 제외 children 없으니 li 로 그냥 접근 */
+$(".m_sub_bg").hide();
+$(".m_menu>li").click(function(){
+  $(this).children(".m_sub_bg").show();
+})
+$(".m_sub_back").click(function(){
+  $(".m_sub_bg").fadeOut(100);
+})
